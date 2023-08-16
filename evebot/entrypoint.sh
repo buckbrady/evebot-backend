3872780469eae867a8f7ba59720cdb407d8267f7
Buck Brady <bbrady145@gmail.com>
2023-08-16 01:21:15 -0600

    chore: add prod dockerfile

--- evebot/entrypoint.sh
+++ evebot/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-if [ "$ENVIRONMENT" == "production" ]; then
-    python3 -m uvicorn evebot/evebot.asgi:application
+if [ "$ENTRY_APP" == "server" ]; then
+  python3 -m uvicorn evebot.asgi:application
+elif [ "$ENTRY_APP" == "worker" ]; then
+  celery -A evebot worker -l info
+elif [ "$ENTRY_APP" == "beat" ]; then
+  celery -A evebot beat -l info
+elif [ "$ENTRY_APP" == "flower" ]; then
+  celery -A evebot flower
 else
-    python3 manage.py runserver 0.0.0.0:8000
+  echo "No entrypoint specified"
 fi
