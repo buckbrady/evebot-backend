492425352508b640d2480e8d56634a3319243a5b
Buck Brady <bbrady145@gmail.com>
2023-08-16 01:25:10 -0600

    chore: fix entrypoint for server

--- evebot/entrypoint.sh
+++ evebot/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$ENTRY_APP" == "server" ]; then
-  python3 -m uvicorn evebot.asgi:application
+  python3 -m uvicorn evebot.asgi:application --host 0.0.0.0 --port 8000 --workers 4
 elif [ "$ENTRY_APP" == "worker" ]; then
   celery -A evebot worker -l info
 elif [ "$ENTRY_APP" == "beat" ]; then
