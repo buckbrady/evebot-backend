bb33dbcc91ad736d26b736d7d313ba9c95461cf6
Buck Brady <bbrady145@gmail.com>
2023-08-16 18:18:45 -0600

    update static files

--- evebot/evebot/settings.py
+++ evebot/evebot/settings.py
@@ -140,6 +140,8 @@ USE_X_FORWARDED_HOST = True
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = "static"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
