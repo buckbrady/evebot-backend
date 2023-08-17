7bcf0627ceaad67b076bcbfbe0d425befbddd91d
Buck Brady <bbrady145@gmail.com>
2023-08-16 18:55:51 -0600

    fix static and csrf

--- evebot/evebot/settings.py
+++ evebot/evebot/settings.py
@@ -19,18 +19,26 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
 environ.Env.read_env()
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-vt3s11-u&i4(!xedgwbz06oh5_f4b&!jq@bajq-j2w1n&a83(#"
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="django-insecure-vt3s11-u&i4(!xedgwbz06oh5_f4b&!jq@bajq-j2w1n&a83(#",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("ENVIRONMENT", default="development") == "development"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost:8000", "evebot.tools"]
 
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8000", "https://evebot.tools"]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:8000",
+    "https://evebot.tools",
+]
 
 # Application definition
 
@@ -140,7 +148,7 @@ USE_X_FORWARDED_HOST = True
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = "static"
+STATIC_ROOT = env("DJANGO_STATIC_ROOT", default="static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
